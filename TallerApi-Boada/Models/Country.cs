4e33aae56cec35480d34c0b27440960615bb2972
laboratorio <laboratorio@UPOAULA10710>
2025-05-05 07:30:15 -0500

    Agregar archivos de proyecto.

---
+++ TallerApi-Boada/Models/Country.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+
+namespace TallerApi_Boada.Models
+{
+    public class Country
+    {
+        public CountryName name { get; set; }
+        public string region { get; set; }
+        public string subregion { get; set; }
+        public ArrayList capital { get; set; }
+    }
+}
---
+++ TallerApi-Boada/Models/CountryName.cs
@@ -0,0 +1,8 @@
+﻿namespace TallerApi_Boada.Models
+{
+    public class CountryName
+    {
+        public string common { get; set; }
+
+    }
+}
